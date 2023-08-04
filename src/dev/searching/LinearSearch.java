fd4afb22980dc2a8d89656795e55f0567738b257
Harini Pavithra <harinipavithra0998@gmail.com>
2023-08-04 19:20:34 +0530

    Add Linear Search

---
+++ src/dev/searching/LinearSearch.java
@@ -0,0 +1,21 @@
+// Linear Search or Sequential Search
+
+import java.io.*;
+
+/*
+Best Case - O(1)
+Average Case - O(N)
+Worst Case - O(N)
+*/
+
+public class LinearSearch {
+    public static int search(int[] arr, int target) {
+        int length = arr.length;
+        for(int i=0;i<length;i++){
+          if(arr[i] == target){
+            return i;
+          }
+        }
+        return -1;
+    }
+}
