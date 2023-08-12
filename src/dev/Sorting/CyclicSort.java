a140b635798d89c9d48d9fec92ba7b6cc62a72d5
Harini Pavithra <harinipavithra0998@gmail.com>
2023-08-12 19:33:05 +0530

    add Cyclic Sort algorithm

---
+++ src/dev/Sorting/CyclicSort.java
@@ -0,0 +1,35 @@
+package dev.harini.sorting;
+
+/*
+Cyclic Sort algorithm
+Best case: O(n)
+Average case: O(n)
+Worst case: O(n)
+Stable: No
+In-place: Yes
+One pass sort/ one loop sort
+Used when the array contains elements in the range 1 to n and the elements are distinct.
+ */
+
+public class CyclicSort {
+    public static int[] cyclicSort(int[] arr){
+        int length = arr.length-1;
+        int i = 0;
+        while(i < length){
+            if(arr[i] != i+1){
+                int correct = arr[i]-1;
+                swapElements(arr, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+        return arr;
+    }
+ 
+  public static void swapElements(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
